b242c785b4e4c882da5ab886164ef9c559a10a72
Michal Kudrnac <michalkud@gmail.com>
2018-11-15 06:57:17 +0100

    Creating of token.

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ const unifiedServer = function (req, res) {
 // Define a request router
 const router = {
   'ping' : handlers.ping,
-  'users' : handlers.users
+  'users' : handlers.users,
+  'tokens' : handlers.tokens
 }
 
